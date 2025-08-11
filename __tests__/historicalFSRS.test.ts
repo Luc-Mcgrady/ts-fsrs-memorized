5002a9ee8a1cee77999ebcf69ef93698ede77e64
Luc Mcgrady <lucmcgrady@gmail.com>
2025-08-11 10:03:22 +0100

    format

--- __tests__/historicalFSRS.test.ts
+++ __tests__/historicalFSRS.test.ts
@@ -1,23 +1,26 @@
-import { createEmptyCard, FSRS } from "ts-fsrs";
-import { historicalFSRS, HistoricalReviewLog } from "../src";
+import { createEmptyCard, FSRS } from "ts-fsrs"
+import { historicalFSRS, HistoricalReviewLog } from "../src"
 
 describe("Historical Memorised", () => {
     it("Same stabilities as ts-fsrs", () => {
-        
         let f = new FSRS({})
         let now = new Date()
         let review2 = now
         review2.setDate(review2.getDate() + 5)
 
-        let revlog: HistoricalReviewLog[] = [{cid: 1, rating: 3, review: now}, {cid: 1, rating: 3, review: review2}]
+        let revlog: HistoricalReviewLog[] = [
+            { cid: 1, rating: 3, review: now },
+            { cid: 1, rating: 3, review: review2 },
+        ]
         let card = createEmptyCard(now)
 
-        historicalFSRS([...revlog], f, 40000, review2, { reviewRangeHook(stability) {
-            let repeat = f.repeat(card, now)[3]
-            let s = repeat.card.stability
-            card = repeat.card
-            expect(s).toBe(stability)
-        }, })
-
+        historicalFSRS([...revlog], f, 40000, review2, {
+            reviewRangeHook(stability) {
+                let repeat = f.repeat(card, now)[3]
+                let s = repeat.card.stability
+                card = repeat.card
+                expect(s).toBe(stability)
+            },
+        })
     })
 })
