76fccc3c63d3852faaceaaa632646924b38f3327
Luc Mcgrady <lucmcgrady@gmail.com>
2025-08-11 11:08:56 +0100

    Added: preset test

--- __tests__/historicalFSRS.test.ts
+++ __tests__/historicalFSRS.test.ts
@@ -1,4 +1,4 @@
-import { createEmptyCard, FSRS } from "ts-fsrs"
+import { Card, createEmptyCard, FSRS, Rating } from "ts-fsrs"
 import { historicalFSRS, HistoricalReviewLog } from "../src"
 
 describe("Historical Memorised", () => {
@@ -16,9 +16,46 @@ describe("Historical Memorised", () => {
 
         historicalFSRS([...revlog], f, 40000, review2, {
             onReviewRange(stability) {
-                let repeat = f.repeat(card, now)[3]
+                let repeat = f.repeat(card, now)[Rating.Good]
                 let s = repeat.card.stability
                 card = repeat.card
+                expect(s).toBe(stability)
+            },
+        })
+    })
+
+    it("Same stabilities with different presets", () => {
+        let f = new FSRS({
+            w: [
+                0.0392, 0.584, 33.4248, 100.0, 7.4532, 0.4596, 2.1257, 0.0013,
+                1.4625, 0.1949, 0.6697, 1.18, 0.1128, 0.2364, 1.7667, 0.1183,
+                1.4849, 0.8357, 0.6337, 0.1707, 0.3,
+            ],
+        })
+        let f2 = new FSRS({})
+
+        let fsrs: Record<number, FSRS> = { 1: f, 2: f2 }
+
+        let now = new Date()
+        let review2 = now
+        review2.setDate(review2.getDate() + 5)
+
+        let revlog: HistoricalReviewLog[] = [
+            { cid: 1, rating: 3, review: now },
+            { cid: 1, rating: 3, review: review2 },
+            { cid: 2, rating: 3, review: now },
+            { cid: 2, rating: 3, review: review2 },
+        ]
+        let cards: Record<number, Card> = {
+            1: createEmptyCard(now),
+            2: createEmptyCard(now),
+        }
+
+        historicalFSRS([...revlog], fsrs, 40000, review2, {
+            onReviewRange(stability, _, __, cid) {
+                let repeat = fsrs[cid].repeat(cards[cid], now)[Rating.Good]
+                let s = repeat.card.stability
+                cards[cid] = repeat.card
                 expect(s).toBe(stability)
             },
         })
